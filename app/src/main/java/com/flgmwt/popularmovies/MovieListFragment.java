9e17b62d8f1d984f6312fbf14dd004e829e99ba4
Ryan Stelly <ryan.stelly@live.com>
2016-10-05 20:50:22 -0500

    feat: Initial commit, fragment w/ WIP menu

---
+++ app/src/main/java/com/flgmwt/popularmovies/MovieListFragment.java
@@ -0,0 +1,34 @@
+package com.flgmwt.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MovieListFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.movie_list, menu);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_movie_list, container);
+
+
+
+        return rootView;
+    }
+}
